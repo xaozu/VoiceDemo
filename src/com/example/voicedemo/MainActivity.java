a216676aae728ffefa3e9ee868d99bdb186ead28
xaozu <zhu425777099@163.com>
2015-06-19 11:22:12 +0800

    初次提交

---
+++ src/com/example/voicedemo/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.voicedemo;
+
+import com.example.voicedemo.service.SoundService;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+/**
+ * 延迟30秒自动播放音乐
+ * @author xaozu
+ *
+ */
+public class MainActivity extends ActionBarActivity {
+
+	public Handler mHandler=new Handler();
+	private static final long POST_PLAY=30*1000;//延迟30秒播放音乐
+	private static final long POST_STOP=60*10000;//播放1分钟停止
+	private TextView text_time;
+	private int time=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        text_time=(TextView)findViewById(R.id.text_time);
+        
+        mHandler.postDelayed(timeRunnable, 1000);//计时
+        mHandler.postDelayed(palyRunnable, 30*1000);//播放
+        mHandler.postDelayed(stopRunnable, POST_PLAY+POST_STOP);//暂停
+        
+    }
+    /**
+     * 更新时间
+     */
+    Handler viewHandler=new Handler(){
+    	public void handleMessage(android.os.Message msg) {
+    		text_time.setText(time+"秒");
+    	};
+    };
+    
+    /**
+     * 在线程中开启播放音乐服务
+     */
+    Runnable timeRunnable=new Runnable() {
+		
+		@Override
+		public void run() {
+			mHandler.postDelayed(timeRunnable, 1000);
+			time++;
+			viewHandler.hasMessages(1);
+		}
+	};
+    
+    
+    /**
+     * 在线程中开启播放音乐服务
+     */
+    Runnable palyRunnable=new Runnable() {
+		
+		@Override
+		public void run() {
+			Intent intent = new Intent(MainActivity.this,SoundService.class);
+            intent.putExtra("playing", true);
+            startService(intent);
+		}
+	};
+	
+	 /**
+     * 在线程中开关闭播放音乐服务
+     */
+    Runnable stopRunnable=new Runnable() {
+		
+		@Override
+		public void run() {
+			Intent intent = new Intent(MainActivity.this,SoundService.class);
+            intent.putExtra("playing", false);
+            startService(intent);
+		}
+	};
+    
+	
+}
---
+++ src/com/example/voicedemo/service/SoundService.java
@@ -0,0 +1,46 @@
+package com.example.voicedemo.service;
+
+import com.example.voicedemo.R;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class SoundService extends Service {
+	    private MediaPlayer mp;
+	 
+	    @Override
+	    public void onCreate() {
+	        super.onCreate();
+	        mp = MediaPlayer.create(this, R.raw.bird);
+	    }
+	 
+	    @Override
+	    public void onDestroy() {
+	        super.onDestroy();
+	        mp.release();
+	        stopSelf();
+	    }
+	 
+	    @Override
+	    public int onStartCommand(Intent intent, int flags, int startId) {
+	        boolean playing = intent.getBooleanExtra("playing", false);
+	        if (playing) {
+	        	Toast.makeText(SoundService.this, "开始播放！", Toast.LENGTH_SHORT).show();
+	            mp.start();
+	        } else {
+	        	Toast.makeText(SoundService.this, "暂停播放！", Toast.LENGTH_SHORT).show();
+	            mp.pause();
+	        }
+	        return super.onStartCommand(intent, flags, startId);
+	    }
+	 
+	    @Override
+	    public IBinder onBind(Intent intent) {
+	        return null;
+	    }
+	 
+	}
+
